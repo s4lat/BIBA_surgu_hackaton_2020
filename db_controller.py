a47562bc060da78a3af2173c33567535eb4d4ad4
Maxim Zakazchik <zakazchikm@gmail.com>
2020-12-05 22:27:06 +0500

    remove_event and get_events_by_lvl

--- db_controller.py
+++ db_controller.py
@@ -10,7 +10,7 @@ class DB:
 		self.events = self.client.test["events"]
 
 	def get_all_events(self):
-		return list(self.events.find({}, {'_id': False}))
+		return list(self.events.find({}, {'_id': False}).sort([['_id', 1]]))
 
 	def insert_event(self, title:str, desc:str, img:str, lvl:int, lon:float, lat:float, cnfrmd:bool):
 		return self.events.insert_one({
@@ -19,14 +19,17 @@ class DB:
 			"img": img,
 			"lvl": lvl,
 			"lon": lon,
+			"lat": lat,
+			"confirmed": cnfrmd
 			}).inserted_id
 	
-	def get_events_by_level(self, lvl:int):
-		pass
+	def get_events_by_lvl(self, lvl:int):
+		return list(self.events.find({"lvl" : lvl}, {'_id': False}))
 
 	def update_event(self, id:int, new:dict):
 		pass
 
-	def remove_event(self, id:int):
-		pass
+	def remove_event(self, _id:int):
+		events = list(self.events.find({}).sort([['_id', 1]]))
+		return self.events.delete_one({"_id" : events[_id-1]["_id"]}).deleted_count
 
